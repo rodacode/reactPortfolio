d4faa521610b2c2162f774ae58ee72a17a7c9071
Nicolas Hardmeier <nicolas.hardmeier@yahoo.es>
2019-11-17 22:06:18 +0100

    added switches

--- src/components/sidenav/Sidenav.js
+++ src/components/sidenav/Sidenav.js
@@ -1,23 +1,35 @@
 import React from 'react';
+import Switch from '@material-ui/core/Switch';
+
 import './sidenav.scss';
 
 const Sidenav = (props) => {
     return (
-        <div>
+        <div className="sidenav">
             <ul>
                 <li>
                     <div className="menu-item-container">
-                        <span className="menu-item">
+                        <span className="sidenav__item">
                             All
-                                        </span>
+                                        </span>      <Switch
+                            defaultChecked
+                            value="checkedF"
+                            color="default"
+                            inputProps={{ 'aria-label': 'checkbox with default color' }}
+                        />
                     </div>
                 </li>
                 <li>
                     <div className="menu-item-container">
-
                         <span className="menu-item">
                             <i className="devicon-angularjs-plain"></i>Angular
                                         </span>
+                        <Switch
+                            defaultChecked
+                            value="checkedF"
+                            color="default"
+                            inputProps={{ 'aria-label': 'checkbox with default color' }}
+                        />
                     </div>
                 </li>
                 <li>
@@ -25,6 +37,12 @@ const Sidenav = (props) => {
                         <span className="menu-item">
                             <i className="devicon-react-plain"></i>React
                                         </span>
+                        <Switch
+                            defaultChecked
+                            value="checkedF"
+                            color="default"
+                            inputProps={{ 'aria-label': 'checkbox with default color' }}
+                        />
                     </div>
                 </li>
                 <li>
@@ -32,6 +50,12 @@ const Sidenav = (props) => {
                         <span className="menu-item">
                             <i className="devicon-vuejs-plain"></i>Vue js
                                         </span>
+                        <Switch
+                            defaultChecked
+                            value="checkedF"
+                            color="default"
+                            inputProps={{ 'aria-label': 'checkbox with default color' }}
+                        />
                     </div>
                 </li>
                 <li>
@@ -39,6 +63,12 @@ const Sidenav = (props) => {
                         <span className="menu-item">
                             <i className="devicon-javascript-plain"></i>Javascript
                                         </span>
+                        <Switch
+                            defaultChecked
+                            value="checkedF"
+                            color="default"
+                            inputProps={{ 'aria-label': 'checkbox with default color' }}
+                        />
                     </div>
                 </li>
                 <li>
@@ -46,6 +76,12 @@ const Sidenav = (props) => {
                         <span className="menu-item">
                             About
                                         </span>
+                        <Switch
+                            defaultChecked
+                            value="checkedF"
+                            color="default"
+                            inputProps={{ 'aria-label': 'checkbox with default color' }}
+                        />
                     </div>
                 </li>
             </ul>
