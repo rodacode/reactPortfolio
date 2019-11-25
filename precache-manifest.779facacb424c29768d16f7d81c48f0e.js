154f010820b2f7716657af9c37b05cb01568f939
Nicolas Hardmeier <nicolas.hardmeier@yahoo.es>
2019-11-25 20:12:05 +0100

    Updates

---
+++ precache-manifest.779facacb424c29768d16f7d81c48f0e.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c0a62b40acf862c41387391fd8e9eed",
+    "url": "/reactPortfolio/index.html"
+  },
+  {
+    "revision": "9877e5c032fa8f8611f0",
+    "url": "/reactPortfolio/static/css/main.4bf9876f.chunk.css"
+  },
+  {
+    "revision": "88b88c952b82477d3d98",
+    "url": "/reactPortfolio/static/js/2.1abfd1d7.chunk.js"
+  },
+  {
+    "revision": "9877e5c032fa8f8611f0",
+    "url": "/reactPortfolio/static/js/main.898452da.chunk.js"
+  },
+  {
+    "revision": "c8586c4cd7e3202f79a2",
+    "url": "/reactPortfolio/static/js/runtime-main.d5b23015.js"
+  },
+  {
+    "revision": "0e84586d2ee5803864e440273e169e38",
+    "url": "/reactPortfolio/static/media/admin_Dashboard.0e84586d.jpg"
+  },
+  {
+    "revision": "dd56d855d3976ff6c4397ac55a5932d8",
+    "url": "/reactPortfolio/static/media/arrow.dd56d855.png"
+  },
+  {
+    "revision": "d3e9d9fd8c4f08f88bd89e543dd72d35",
+    "url": "/reactPortfolio/static/media/chatroom.d3e9d9fd.jpg"
+  },
+  {
+    "revision": "3808b017f3b7023a472335fd78db1bce",
+    "url": "/reactPortfolio/static/media/derdiedas.3808b017.jpg"
+  },
+  {
+    "revision": "5da48bb10532fb046907f81fef059693",
+    "url": "/reactPortfolio/static/media/developer.5da48bb1.png"
+  },
+  {
+    "revision": "c532b91ac4e1793602ec6edb3e1aea5f",
+    "url": "/reactPortfolio/static/media/dub-siren.c532b91a.png"
+  },
+  {
+    "revision": "0916317b90841de17cd2dac124ca9831",
+    "url": "/reactPortfolio/static/media/envases.0916317b.jpg"
+  },
+  {
+    "revision": "c4e2b312717849447c428debbcdec5c3",
+    "url": "/reactPortfolio/static/media/leparfum.c4e2b312.png"
+  },
+  {
+    "revision": "39b69b7a4b22485a9529da729c52f4df",
+    "url": "/reactPortfolio/static/media/lorena_canals.39b69b7a.jpg"
+  },
+  {
+    "revision": "4eee72f9f0be5df4f569cf2fa601b33e",
+    "url": "/reactPortfolio/static/media/ng_cms.4eee72f9.jpg"
+  },
+  {
+    "revision": "22b38018810f5f5a96f313cdbb99fe64",
+    "url": "/reactPortfolio/static/media/portfolio.22b38018.png"
+  },
+  {
+    "revision": "a28c39edf7bbba583d943e5f0d82a455",
+    "url": "/reactPortfolio/static/media/tacitus_drum_machine.a28c39ed.png"
+  },
+  {
+    "revision": "769e45f566d7d0968880bf58a1e34387",
+    "url": "/reactPortfolio/static/media/youtubedj.769e45f5.jpg"
+  }
+]);
